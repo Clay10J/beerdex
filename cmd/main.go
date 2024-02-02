9604365a097f30afcc9241027fce2a72dd0d6e62
clayjoseck <clayjoseck@yahoo.com>
2024-02-02 01:28:44 -0600

    Add default air config

--- cmd/main.go
+++ cmd/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"log"
+	"net/http"
 	"os"
 
 	"github.com/joho/godotenv"
@@ -22,7 +23,13 @@ func main() {
 	}
 
 	e := echo.New()
+
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
+
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, World!")
+	})
+
 	e.Logger.Fatal(e.Start(fmt.Sprintf(":%v", port)))
 }
